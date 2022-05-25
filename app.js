cb19d5ef73f9e2230f40d3d3fe37cd7d0e8d5e3f
*Segerkung* <*leo.segerljung@elev.ga.ntig.se*>
2022-05-25 13:32:30 +0200

    AAAAAAAA funkar inte :DDDDDDD

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app
@@ -29,5 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 module.exports = app;
