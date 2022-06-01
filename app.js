b482951c352fafe0d7506d5efa2b0e0b4c322a97
*Segerkung* <*leo.segerljung@elev.ga.ntig.se*>
2022-06-01 12:52:07 +0200

    ✨ GET

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/denied', deniedRouter);
+app.use('/secret', secretRouter);
+
 
 
 
