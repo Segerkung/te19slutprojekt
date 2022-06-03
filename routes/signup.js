a080cb18659bab6339e5a122f4b0c5cd19d0338b
*Segerkung* <*leo.segerljung@elev.ga.ntig.se*>
2022-06-03 10:03:40 +0200

    ✨ GET RROUTES login, signup, index.

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../database');
+const bcrypt = require('bcrypt');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.njk', { title: 'hej' });
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  let data = {
+    layout: 'layout.njk'
+  }
+  res.render('signup.njk', data);
 });
+
+router.post('/', async (req, res, next) => {
+  const username = req.body.name;
+  let password = req.body.password;
+  bcrypt.hash(password, 8, async function (err, hash) {
+    await pool.promise()
+      .query('INSERT INTO leoseg_songs (user,password) VALUES (?,?)', [username, hash])
+      .then((response) => {
+        console.log(response);
+        if (response[0].affectedRows === 1) {
+          res.redirect('/login');
+        } else {
+          res.status(400).redirect('/signup');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect('/signup');
+      });
+  });
+});
+
 
 module.exports = router;
