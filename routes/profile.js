21d932b2b117eb5e78a54818669ce0916590d6a3
*Segerkung* <*leo.segerljung@elev.ga.ntig.se*>
2022-06-08 22:03:09 +0200

    ✨ css

--- routes/profile.js
+++ routes/profile.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res, next) => {
         });
 });
 
-router.post('/', async (req, res, next) => {
+ router.post('/', async (req, res, next) => {
     const song = req.body.song;
 
     await pool.promise()
@@ -49,6 +49,7 @@ router.post('/', async (req, res, next) => {
         })
     });
 });
+
 
 router.get('/:id/delete', async (req,res,next) => {
     const id = req.params.id;
